5a1dcf0931ff900732308cd679dcdebe0f23372b
liamza1 <liam.streak@gmail.com>
2017-10-19 15:20:19 +0200

    first
    first

---
+++ php/database.php
@@ -0,0 +1,6 @@
+<?php
+    $DB_BASE = 'camagru';
+    $DB_DSN = 'mysql:host=127.0.0.1;dbname='.$DB_BASE;
+    $DB_USER = 'root';
+    $DB_PASSWORD = 'ciricarl';
+?>
---
+++ php/datafixture.php
@@ -0,0 +1,4 @@
+<?php
+    require_once('database.php');
+    $db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+echo "Done !<br><br>";
